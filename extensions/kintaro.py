8b007aa27c870b2fbcd305bef4e60b602efd17ec
Jeremy Weinstein <jeremydw@gmail.com>
2016-07-26 13:34:20 -0700

    A few minor fixes to the Kintaro extension.

--- extensions/kintaro.py
+++ extensions/kintaro.py
@@ -8,18 +8,17 @@ import grow
 import httplib2
 
 
-STORAGE_KEY = 'Grow SDK - Kintaro'
-KINTARO_HOST = 'https://kintaro-content-server-qa.appspot.com'
-KINTARO_API_ROOT = '{host}/_ah/api'.format(host=KINTARO_HOST)
+KINTARO_HOST = 'kintaro-content-server-qa.appspot.com'
+KINTARO_API_ROOT = 'https://{host}/_ah/api'.format(host=KINTARO_HOST)
 DISCOVERY_URL = (
     KINTARO_API_ROOT + '/discovery/v1/apis/{api}/{apiVersion}/rest')
 KINTARO_EDIT_PATH_FORMAT = (
-    '{host}'
+    'https://{host}'
     '/app#/project/{project}'
     '/repo/{repo}'
     '/collection/{collection}'
     '/document/{document}/edit')
-
+STORAGE_KEY = 'Grow SDK - Kintaro'
 
 # Silence extra logging from googleapiclient.
 discovery.logger.setLevel(logging.WARNING)
@@ -40,17 +39,23 @@ class KintaroPreprocessor(grow.Preprocessor):
         bind = messages.MessageField(BindingMessage, 1, repeated=True)
         repo = messages.StringField(2)
         project = messages.StringField(3)
+        host = messages.StringField(4, default=KINTARO_HOST)
 
     @staticmethod
-    def create_service():
+    def create_service(host):
         credentials = oauth.get_or_create_credentials(
             scope=OAUTH_SCOPE, storage_key=STORAGE_KEY)
         http = httplib2.Http(ca_certs=utils.get_cacerts_path())
         http = credentials.authorize(http)
+        # Kintaro's server doesn't seem to be able to refresh expired tokens
+        # properly (responds with a "Stateless token expired" error).  So for
+        # now, automatically refresh tokens each time a service is created. If
+        # this isn't fixed on the Kintaro end, what we can do is implement our
+        # own refresh system (tokens need to be refreshed once per hour).
         credentials.refresh(http)
-        return discovery.build(
-            'content', 'v1', http=http,
-            discoveryServiceUrl=DISCOVERY_URL)
+        url = DISCOVERY_URL.replace('{host}', host)
+        return discovery.build('content', 'v1', http=http,
+                               discoveryServiceUrl=url)
 
     def bind_collection(self, entries, collection_pod_path):
         collection = self.pod.get_collection(collection_pod_path)
@@ -82,17 +87,17 @@ class KintaroPreprocessor(grow.Preprocessor):
         return fields, body, basename
 
     def run(self, *args, **kwargs):
-      for binding in self.config.bind:
-          collection_pod_path = binding.collection
-          kintaro_collection = binding.kintaro_collection
-          entries = self.download_entries(
-              repo_id=self.config.repo,
-              collection_id=kintaro_collection,
-              project_id=self.config.project)
-          self.bind_collection(entries, collection_pod_path)
+        for binding in self.config.bind:
+            collection_pod_path = binding.collection
+            kintaro_collection = binding.kintaro_collection
+            entries = self.download_entries(
+                repo_id=self.config.repo,
+                collection_id=kintaro_collection,
+                project_id=self.config.project)
+            self.bind_collection(entries, collection_pod_path)
 
     def download_entries(self, repo_id, collection_id, project_id):
-        service = KintaroPreprocessor.create_service()
+        service = KintaroPreprocessor.create_service(host=self.config.host)
         resp = service.documents().listDocuments(
             repo_id=repo_id,
             collection_id=collection_id,
@@ -100,7 +105,7 @@ class KintaroPreprocessor(grow.Preprocessor):
         return resp.get('documents', [])
 
     def download_entry(self, document_id, collection_id, repo_id, project_id):
-        service = KintaroPreprocessor.create_service()
+        service = KintaroPreprocessor.create_service(host=self.config.host)
         resp = service.documents().getDocument(
             document_id=document_id,
             collection_id=collection_id,
@@ -115,11 +120,11 @@ class KintaroPreprocessor(grow.Preprocessor):
             if binding.collection == doc.collection.pod_path:
                 kintaro_collection = binding.kintaro_collection
         return KINTARO_EDIT_PATH_FORMAT.format(
-              host=KINTARO_HOST,
-              project=self.config.project,
-              repo=self.config.repo,
-              collection=kintaro_collection,
-              document=kintaro_document)
+            host=self.config.host,
+            project=self.config.project,
+            repo=self.config.repo,
+            collection=kintaro_collection,
+            document=kintaro_document)
 
     def can_inject(self, doc=None, collection=None):
         if not self.injected:
@@ -138,6 +143,6 @@ class KintaroPreprocessor(grow.Preprocessor):
                     collection_id=binding.kintaro_collection,
                     repo_id=self.config.repo,
                     project_id=self.config.project)
-                fields, body, basename = self._parse_entry(entry)
+                fields, _, _ = self._parse_entry(entry)
                 doc.inject(fields, body='')
                 return doc
